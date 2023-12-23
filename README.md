840c8c13870586cc5dce4308afcdeda690b61149
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2023-12-23 17:41:19 +0900

    :tada: Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learning Python with Flet
+
+Introduction to Python with the framework `Flet`.
+
+[Flet](https://flet.dev/)
---
+++ counter.py
@@ -0,0 +1,30 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Flet Counter"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    text_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        text_number.value = str(int(text_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        text_number.value = str(int(text_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            controls=[
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                text_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+
+
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
